80abb15023fa6bd8ed60e9c5c092c4bda329f404
Jeeva Gowtham <itszva1436@gmail.com>
2022-12-13 08:31:56 +0000

    Output

--- webserver.py
+++ webserver.py
@@ -1,21 +1,3 @@
-# Developing a Simple Webserver
-## AIM:
-To develop a simple webserver to display top five frameworks.
-
-## DESIGN STEPS:
-### Step 1: 
-HTML content creation
-### Step 2:
-Design of webserver workflow
-### Step 3:
-Implementation using Python code
-### Step 4:
-Serving the HTML pages.
-### Step 5:
-Testing the webserver
-
-## PROGRAM:
-```
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 content = """
@@ -48,10 +30,3 @@ class HelloHandler(BaseHTTPRequestHandler):
 server_address = ('',80)
 httpd = HTTPServer(server_address, HelloHandler)
 httpd.serve_forever()
-```
-
-
-## OUTPUT:
-
-
-## RESULT:
